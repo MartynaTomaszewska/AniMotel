a1e0e5fe6b34957e07901422549fb0e812e65ae7
Martyna Tomaszewska <martyna.tomaszewska00@gmail.com>
2019-01-30 19:08:36 +0100

    Added Angular frontend and created landing page with transparent header.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 
 
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,81 @@
+import * as M from 'materialize-css';
+
+import { Component, OnInit, AfterViewInit, HostListener, ViewChild, ElementRef } from '@angular/core';
+import { Router } from '@angular/router';
+import { map, flatMap } from 'rxjs/operators';
+
+//import * as M from 'materialize-css/dist/js/materialize';
+
+//import { MaterializeAction, MaterializeDirective } from 'angular2-materialize';
+declare var $: any;
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit, AfterViewInit {
+
+  private isScrolled = false;
+
+  //closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+
+  constructor(private router: Router) { }
+
+  public showSideNav() {
+    //this.sideNavActions.emit({ action: 'sideNav', params: ['show'] });
+  }
+
+  public hideSideNav() {
+    //this.sideNavActions.emit({ action: 'sideNav', params: ['hide'] });
+  }
+
+  ngOnInit() {
+
+  }
+
+  ngAfterViewInit() {
+    $('.sidenav').sidenav();
+  }
+
+  headerClasses() {
+    return {
+      'header--scrolled': this.isScrolled,
+      'header--transparent': true
+    };
+  }
+
+  logoImg() {
+    return 'assets/img/logoWhite.png';
+  }
+
+  navigateToLanding() {
+    this.router.navigate([""]);
+  }
+
+  selectModel(modelId) {
+  
+  }
+
+  selectConfiguratorStep(step) {
+   
+  }
+
+  getModelsNames() {
+  
+  }
+
+ 
+
+  private onLandingPage() {
+    return this.router.isActive('/', true);
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  private onWindowScroll(event: any) {
+    const height = event.target.documentElement.clientHeight;
+    const scroll = event.target.documentElement.scrollTop;
+    //this.isScrolled = scroll / height > 0.1;
+  }
+
+}
